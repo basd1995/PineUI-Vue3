5c62b84f4d3b7aefd065d82cf7967d96817e3290
basd1995 <1035102183@qq.com>
2020-09-24 15:53:53 +0800

    实现 aside 切换

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <router-view />
+  </div>
+</template>
+
+<script lang="ts">
+import { provide, ref } from 'vue'
+export default {
+  name: 'App',
+  setup() {
+    const menuVisible = ref(false)
+    provide('xxx', menuVisible)
+  }
+}
+</script>
