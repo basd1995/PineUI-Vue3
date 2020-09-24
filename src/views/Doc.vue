11d234dae55f4213ded82b1de3830013e5c891ac
basd1995 <1035102183@qq.com>
2020-09-24 16:11:01 +0800

    完善 aside 切换逻辑

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -36,10 +36,6 @@ aside {
   background: lightblue;
   width: 150px;
   padding: 16px;
-  position: fixed;
-  top: 0;
-  left: 0;
-  padding-top: 70px;
   > h2 {
     margin-bottom: 4px;
   }
@@ -47,6 +43,13 @@ aside {
     > li {
       padding: 4px 0;
     }
+  }
+
+  @media screen {
+    position: fixed;
+    top: 0;
+    left: 0;
+    padding-top: 70px;
   }
 }
 </style>
