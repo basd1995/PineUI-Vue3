b85bc9f2ea2b6801d5eda4c7c70dfbcba21ed973
basd1995 <1035102183@qq.com>
2020-09-27 16:37:23 +0800

    整理scss

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -28,27 +28,27 @@ export default {
     border-radius: $h/2;
     position: relative;
     outline: none;
-  }
-  span{
-    position: absolute;
-    top: 2px;
-    left: 2px;
-    height: $h2;
-    width: $h2;
-    background:white;
-    border-radius: $h2 / 2;
-    transition: all 250ms;
-  }
-  button.checked {
-    background: #1880ff;
-  }
-  button.checked > span {
-    left: calc(100% - #{$h2} - 2px);
-  }
-  button:active{
-    > span {width: $h2 + 4px;}
-  }
-  button.checked:active{
-    > span {width: $h2 + 4px; margin-left: -4px;}
+    > span{
+      position: absolute;
+      top: 2px;
+      left: 2px;
+      height: $h2;
+      width: $h2;
+      background:white;
+      border-radius: $h2 / 2;
+      transition: all 250ms;
+    }
+    &.checked {
+      background: #1880ff;
+      > span {
+        left: calc(100% - #{$h2} - 2px);
+      }
+    }
+    &:active{
+      > span {width: $h2 + 4px;}
+    }
+    &.checked:active{
+      > span {width: $h2 + 4px; margin-left: -4px;}
+    }
   }
 </style>
