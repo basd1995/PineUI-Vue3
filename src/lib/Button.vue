8654e4651c375820fb10b79d8ff94291eb2d4450
basd1995 <1035102183@qq.com>
2020-10-10 22:22:03 +0800

    支持size和level

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,15 +1,35 @@
 <template>
-  <button class="pine-button" :class="`pine-theme-${theme}}`">
+  <button class="pine-button" :class="classes">
     <slot />
   </button>
 </template>
 <script>
+import { computed } from 'vue'
 export default {
   props: {
     theme: {
       type: String,
       default: 'button'
+    },
+    size: {
+      type: String,
+      default: 'normal' 
+    },
+    level: {
+      type: String,
+      default: 'normal'
     }
+  },
+  setup(props) {
+    const { theme, size, level } = props;
+    const classes = computed(() => {
+      return { 
+        [`pine-theme-${theme}`]: theme,
+        [`pine-size-${size}`]: size,
+        [`pine-level-${level}`]: level,
+      }
+    })
+    return { classes }
   }
 }
 </script>
@@ -19,6 +39,7 @@ export default {
   $border-color: #d9d9d9;
   $color: #333;
   $blue: #40a9ff;
+  $red: red;
   $radius: 4px;
   .pine-button {
     box-sizing: border-box;
@@ -34,6 +55,7 @@ export default {
     border: 1px solid $border-color;
     border-radius: $radius;
     box-shadow: 0 1px 0 fade-out(black, 0.95);
+    transition: background 250ms;
     & + & {
       margin-left: 8px;
     }
@@ -47,6 +69,79 @@ export default {
     }
     &::-moz-focus-inner {
       border: 0;
+    }
+    &.pine-theme-link {
+      border-color: transparent;
+      box-shadow: none;
+      color: $blue;
+      &:hover,&:focus{
+        color: lighten($blue, 10%);
+      }
+    }
+    &.pine-theme-text {
+      border-color: transparent;
+      box-shadow: none;
+      color: inherit;
+      &:hover,&:focus{
+        background: darken(white, 5%);
+      }
+    }
+    &.pine-size-big {
+      font-size: 24px;
+      height: 48px;
+      padding: 0 16px;
+    }
+    &.pine-size-small {
+      font-size: 12px;
+      height: 20px;
+      padding: 0 4px;
+    }
+    &.pine-theme-button {
+      &.pine-level-main {
+        background: $blue;
+        color: white;
+        border-color: $blue;
+        &:hover,
+        &:focus {
+          background: darken($blue, 10%);
+          border-color: darken($blue, 10%);
+        }
+      }
+      &.pine-level-danger {
+        background: $red;
+        border-color: $red;
+        color: white;
+        &:hover,
+        &:focus {
+          background: darken($red, 10%);
+          border-color: darken($red, 10%);
+        }
+      }
+    }
+    &.pine-theme-link {
+      &.pine-level-danger {
+        color: $red;
+        &:hover,
+        &:focus {
+          color: darken($red, 10%);
+        }
+      }
+    }
+    &.pine-theme-text {
+      &.pine-level-main {
+        color: $blue;
+        &:hover,
+        &:focus {
+          color: darken($blue, 10%);
+        }
+      }
+      &.pine-level-danger {
+        color: $red;
+        &:hover,
+        &:focus {
+          color: darken($red, 10%);
+        }
+      }
     }
   }
 </style>
